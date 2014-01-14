23b72332e6505096d23e2106f6fb8647f04759c9
benjamin_bonkov <benjamin-bonnetkovacs@cifacom.com>
2014-01-14 14:23:23 +0100

    update/create avec champs optionnels suppression

--- api/controllers/FIlmsController.php
+++ api/controllers/FIlmsController.php
@@ -49,9 +49,19 @@ class FilmsController{
 	}
 
 	public function actionCreate(){
-		if(isset($_POST['nomFilm'])){
-			$data = array('création de '.$_POST["nomFilm"].')');
-			$this->db->exec('INSERT INTO `film` (nomFilm) VALUES("'.$_POST["nomFilm"].'")');
+		if(isset(F3::get('POST')['nomFilm'])){
+			$values = '';
+			$req = '';
+			foreach(F3::get('POST') as $key => $value){
+				if($values != ''){
+					$values .= ' , ';
+					$req .= ' , ';
+				}
+				$req .= '`'.$key.'`';
+				$values .= '"'.$value.'"';
+			}
+			$data = array('création de '.$_POST["nomFilm"].'');
+			$this->db->exec('INSERT INTO `film` ('.$req.') VALUES('.$values.');');
 			Api::response(200, $data);
 		}
 		else{
@@ -61,13 +71,21 @@ class FilmsController{
 
 
 	public function actionUpdate(){
-		$data = array('Update film with id : ' . F3::get('PARAMS.id').' set name ='.F3::get('GET.nomFilm'));
-		$this->db->exec('UPDATE  `film` SET  `nomFilm` =  "'.F3::get('GET.nomFilm').'" WHERE  `film`.`idFilm` ='.F3::get('PARAMS.id').';');
+		$req = '';
+		foreach(F3::get('GET') as $key => $value){
+			if($req != ''){
+				$req .= ' , ';
+			}
+			$req .= '`'.$key.'`="'.$value.'"';
+		}
+		$data = array('Update film with id : ' . F3::get('PARAMS.id'));
+		$this->db->exec('UPDATE  `film` SET  '.$req.' WHERE  `film`.`idFilm` ='.F3::get('PARAMS.id').';');
 		Api::response(200, $data);
 	}
 
 	public function actionDelete(){
 		$data = array('Delete film with name: ' . F3::get('PARAMS.id'));
+		$this->db->exec('DELETE FROM `film` WHERE `film`.`idFilm` ='.F3::get('PARAMS.id').';');
 		Api::response(200, $data);
 	}
 }
